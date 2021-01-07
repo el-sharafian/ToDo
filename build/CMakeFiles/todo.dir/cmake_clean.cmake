fd3361ed7a8f74162ff94f7f310e0543b33a1fc1
el-sharafian <76821267+el-sharafian@users.noreply.github.com>
2021-01-07 22:42:48 +0330

    Add files via upload

---
+++ build/CMakeFiles/todo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/todo.dir/src/main.cpp.o"
+  "CMakeFiles/todo.dir/src/start.cpp.o"
+  "todo"
+  "todo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/todo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/start.hpp
@@ -0,0 +1,6 @@
+#ifndef START_HPP
+#define START_HPP
+
+void start();
+
+#endif // START_HPP
